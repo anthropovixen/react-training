17263a990e6491e7d86222dc5c2620e63e9cd33c
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-18 14:37:35 +1100

    Update index.js
    Change name of component
    Add header
    Add navbar
    Add footer

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-function TemporaryName() {
+function Page() {
   return (
     <div>
-      <img src="./react-logo.png" width="40px" />
-      <h1>Fun Facts about React</h1>
+      <header>
+        <nav>
+          <img src="./react-logo.png" width="40px" />
+        </nav>
+      </header>
+      <h1>Reasons why I'm excited to learn React</h1>
       <ul>
         <li> Was first released in 2013</li>
         <li> Was originally created by Jordan Walke</li>
         <li> Has well over 100k stars on GitHub</li>
         <li> Is mantained by Facebook</li>
-              <li> Powers thousands of enterprise apps, including mobile apps</li>
+        <li> Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
+      <footer>
+        <small>©2022 Santos development. All rights reserved.</small>{" "}
+      </footer>
     </div>
   );
 }
 
-ReactDOM.render(<TemporaryName />, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
