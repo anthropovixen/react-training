aba3d0c96cbbf6222bf43cbd00333c7c5efecda0
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-18 15:37:30 +1100

    Update index.js
    Separate content of the page into components
    Header
    MainContent
    Footer

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-function Page() {
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img className="nav-image" src="./react-logo.png" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./react-logo.png" width="40px" />
-        </nav>
-      </header>
       <h1>Reasons why I'm excited to learn React</h1>
-      <ul>
-        <li> Was first released in 2013</li>
-        <li> Was originally created by Jordan Walke</li>
+      <ol>
+        <li> It's cool!</li>
         <li> Has well over 100k stars on GitHub</li>
         <li> Is mantained by Facebook</li>
         <li> Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
-      <footer>
-        <small>©2022 Santos development. All rights reserved.</small>{" "}
-      </footer>
+      </ol>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>©2022 Santos development. All rights reserved.</small>{" "}
+    </footer>
+  );
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
