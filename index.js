5c0fc9af2d6ba866cf45e5eaf5254e11d902bbc6
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-18 14:30:40 +1100

    Update index.js
    Create first component and render it in the DOM

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const page = (
-  <div>
-    <img src="./react-logo.png" width="40"></img>
-    <h1>Fun Facts about React</h1>
-    <ul>
-      <li> Was first released in 2013</li>
-      <li> Was originally created by Jordan Walke</li>
-      <li> Has well over 100k stars on GitHub</li>
-      <li> Is mantained by Facebook</li>
-      <li> Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
+function TemporaryName() {
+  return (
+    <div>
+      <img src="./react-logo.png" width="40px" />
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li> Was first released in 2013</li>
+        <li> Was originally created by Jordan Walke</li>
+        <li> Has well over 100k stars on GitHub</li>
+        <li> Is mantained by Facebook</li>
+              <li> Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<TemporaryName />, document.getElementById("root"));
