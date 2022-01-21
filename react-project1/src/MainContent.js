a4911bb057f31bdf5b0cdf4989d8df5328facdb7
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-21 14:53:44 +1100

    Create MainContent.js
    Separate MainContent in a different file

---
+++ react-project1/src/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MainContent() {
+  return (
+    <div>
+      <h1>Reasons why I'm excited to learn React</h1>
+      <ol>
+        <li> It's cool!</li>
+        <li> Has well over 100k stars on GitHub</li>
+        <li> Is mantained by Facebook</li>
+        <li> Powers thousands of enterprise apps, including mobile apps</li>
+      </ol>
+    </div>
+  );
+}
