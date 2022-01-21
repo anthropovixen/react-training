295919d10bf4f0f693f10a92f78098909b40abc7
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-21 14:53:32 +1100

    Update index.js
    Remove components from inside index.js to import them from separate files

--- react-project1/src/index.js
+++ react-project1/src/index.js
@@ -1,28 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
+import MainContent from "./MainContent";
 
-function MainContent() {
-  return (
-    <div>
-      <h1>Reasons why I'm excited to learn React</h1>
-      <ol>
-        <li> It's cool!</li>
-        <li> Has well over 100k stars on GitHub</li>
-        <li> Is mantained by Facebook</li>
-        <li> Powers thousands of enterprise apps, including mobile apps</li>
-      </ol>
-    </div>
-  );
-}
 
-function Footer() {
-  return (
-    <footer>
-      <small>©2022 Santos development. All rights reserved.</small>
-    </footer>
-  );
-}
+
+
 
 function Page() {
   return (
