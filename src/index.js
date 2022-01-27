6b7de9498c517f6c55b998a83c635c7131adcd86
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-28 10:40:43 +1100

    Update index.js
    Remove footer from App

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 import MainContent from "./MainContent";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <MainContent />
-      <Footer />
     </div>
   );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
