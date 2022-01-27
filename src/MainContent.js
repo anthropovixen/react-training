2beec2b4e5bc5d5affa34e8fbcd08cc351cecc76
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-28 10:42:50 +1100

    Update MainContent.js
    Change text for the app
    change classNames
    Remove ordered list and add unordered list
    Remove div and put main

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function MainContent() {
   return (
-    <div className="body">
-      <h1>Reasons why I'm excited to learn React</h1>
-      <ol>
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--list">
         <li> It's cool!</li>
         <li> Has well over 100k stars on GitHub</li>
         <li> Is mantained by Facebook</li>
         <li> Powers thousands of enterprise apps, including mobile apps</li>
-      </ol>
-    </div>
+      </ul>
+    </main>
   );
 }
