906b05ef70493b6adc7e8d6e705df86a2b1e3308
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-28 10:41:44 +1100

    Update Header.js
    Change structure of component to make it similar to the one at the course (for practice purposes)
    Remove list of items from navigation bar and change texts

--- src/Header.js
+++ src/Header.js
@@ -3,15 +3,10 @@ import logo512 from "./images/logo512.png";
 
 export default function Header() {
   return (
-    <header className="header">
-      <nav className="nav">
-        <img className="nav-logo" src={logo512} alt="react logo" />
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
+    <nav>
+      <img className="nav--logo" src={logo512} alt="react logo" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
   );
 }
