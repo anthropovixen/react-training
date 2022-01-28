21e94cd837377e8c8200e798cf8f45cc0ca1aebf
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-28 12:22:25 +1100

    Update index.js
    Remove footer component
    Import style.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
 import MainContent from "./MainContent";
-// import Footer from "./Footer";
+import "./style.css";
 
 function App() {
   return (
