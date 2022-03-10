ce59b9a17673ca1d6dceb61073d917a0d87c3abf
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-10 13:53:19 +1100

    Create App.js
    Add dark mode by:
    creating state
    creating function to toggle from and to dark mode
    adding darkmode properties to components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+export default function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !prevMode);
+  }
+  return (
+    <div className="container">
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Main darkMode={darkMode} />
+    </div>
+  );
+}
