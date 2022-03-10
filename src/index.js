fa50eb87243f4472d94ea87408f6475a3f0a5980
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-10 13:56:14 +1100

    Update index.js
    Change index.js to create app.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./Header";
-import MainContent from "./MainContent";
-import "./style.css";
-
-function App() {
-  return (
-    <div className="container">
-      <Header />
-      <MainContent />
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
